cc21f542e9a7a0b88cc6a9622c5163ad80d3414f
Mikhail Egorov <72557814+Shonmikl@users.noreply.github.com>
2022-11-29 15:12:53 +0300

    Update Bottom.java

--- src/main/java/model/Bottom.java
+++ src/main/java/model/Bottom.java
@@ -4,11 +4,11 @@ import org.example.ImproveTask;
 import org.example.Position;
 
 public class Bottom extends Employee implements  ImproveTask {
-    public Position p;
+    public Position p; //что за "p"?
     public Bottom(String name, int age, Position p) {
         super(name, age);
         this.p = p;
-
+//не нужная строка
     }
 
     public Position getP() {
@@ -20,3 +20,4 @@ public class Bottom extends Employee implements  ImproveTask {
         System.out.println("Выполняю задание");
     }
 }
+//не нужная строка
