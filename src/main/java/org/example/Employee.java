6688c5a24da049507a0ebc6a3bb3c624d2fddacb
Mikhail Egorov <72557814+Shonmikl@users.noreply.github.com>
2022-11-29 15:13:32 +0300

    Update Employee.java

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -3,9 +3,9 @@ package org.example;
 public class Employee {
     String name;
     int age;
-
-
-
+//не нужная строка
+//не нужная строка
+//не нужная строка
 
     public Employee(String name, int age) {
         this.name = name;
@@ -20,8 +20,8 @@ public class Employee {
     public int getAge() {
         return age;
     }
-
-
+//не нужная строка
+//не нужная строка
 
     public void setName(String name) {
         this.name = name;
@@ -30,5 +30,6 @@ public class Employee {
     public void setAge(int age) {
         this.age = age;
     }
-
+//не нужная строка
 }
+//не нужная строка
