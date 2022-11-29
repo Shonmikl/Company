95018d27ef258171c6dfe42b9e4b26d9e7f70cf1
Mikhail Egorov <72557814+Shonmikl@users.noreply.github.com>
2022-11-29 15:14:32 +0300

    Update Service.java

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -8,14 +8,17 @@ import org.example.Position;
 public class Service {
     public static void changePosition(Top top) {
         top.p = Position.middle;
-
+//не нужная строка
     }
+    
     public static void changePosition1(Middle middle) {
         middle.p = Position.top;
-
+//не нужная строка
     }
+    
     public static void changePosition2(Bottom bottom) {
         bottom.p = Position.middle;
-
+//не нужная строка
     }
 }
+//не нужная строка
